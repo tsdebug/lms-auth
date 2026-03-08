830b5cc1632d9dcbbfa0df6c31344b81ed4c07fc
Tsdebug <taworkspace01@gmail.com>
2026-03-08 20:26:44 +0530

    Removed myFunction to remove unnecessary errors

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -10,7 +10,6 @@
 
 import type * as auth from "../auth.js";
 import type * as http from "../http.js";
-import type * as myFunctions from "../myFunctions.js";
 
 import type {
   ApiFromModules,
@@ -21,7 +20,6 @@ import type {
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
   http: typeof http;
-  myFunctions: typeof myFunctions;
 }>;
 
 /**
