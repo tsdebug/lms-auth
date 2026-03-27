751e92bb22710ac9b5fec8e8a446688427e11640
Tsdebug <taworkspace01@gmail.com>
2026-03-28 03:01:55 +0530

    Added mutation function for courses - updateCourse (2 parts are left)

--- convex/courses/mutations.ts
+++ convex/courses/mutations.ts
@@ -2,6 +2,7 @@ import { v } from "convex/values";
 import { mutation } from "../_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
+// createCourse - called when a teacher creates a new course, to save the course details and link it to the creator as the lead instructor
 export const createCourse = mutation({
     args: {
         // what does a course need at creation time? - hint: look at the courses table in schema.ts
@@ -71,5 +72,43 @@ export const createCourse = mutation({
             createdAt: Date.now(),
             updatedAt: Date.now(),
         });
+    },
+});
+
+//  updateCourse - called when a teacher updates their course details, to update the course info in the courses table
+// idea - if the course i am trying to update exists and if i am authenticated as well as authorized - instructor or co-instructor on this course
+export const updateCourse = mutation({
+    args: {
+        courseId: v.id("courses"),
+        title: v.optional(v.string()),
+        description: v.optional(v.string()),
+        thumbnailUrl: v.optional(v.string()),   // PRD §4
+        slug: v.optional(v.string()),
+        difficultyLevel: v.optional(
+        v.union(v.literal("beginner"), v.literal("intermediate"), v.literal("advanced"))
+    ),
+    },
+
+    handler: async (ctx, args) => {
+        // 1. auth check
+        const authUserId = await getAuthUserId(ctx);
+        if (!authUserId) {
+            throw new Error("User must be authenticated");
+        }
+
+        // 2. does the course even exist?
+        const existingCourse = await ctx.db.get(args.courseId);
+        if (!existingCourse) {
+            throw new Error("Course not found");
+        }
+
+        // 3. role check 
+        const isInstructor = await ctx.db
+            .query("course_instructors") // if a row exists here for a user & course, that means they are an instructor on that course
+            .withIndex("courseId_userId", (q)=> q.eq("courseId", args.courseId).eq("userId", authUserId))
+            .first();
+            if(!isInstructor){
+                throw new Error("Unauthorized: only instructors can update courses");
+            }
     },
 });
