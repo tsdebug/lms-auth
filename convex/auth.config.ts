cdaff157ed764fdb5529bd1c31644cfdde35b36d
Tsdebug <taworkspace01@gmail.com>
2026-04-04 01:34:36 +0530

    Modified domain

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from "convex/server";
 export default {
   providers: [
     {
-      domain: process.env.CONVEX_SITE_URL!,
+      domain: process.env.CONVEX_SITE_URL ?? process.env.NEXT_PUBLIC_CONVEX_SITE_URL!,
       applicationID: "convex",
     },
   ],
