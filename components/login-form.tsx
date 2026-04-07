a317900d7bb88f5858ec2101f62db1d8002b338d
Tsdebug <taworkspace01@gmail.com>
2026-04-08 00:22:15 +0530

    Modified onclick function

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -88,7 +88,7 @@ export function LoginForm({
           <Button
             variant="outline"
             type="button"
-            onClick={() => signIn("github")}
+            onClick={() => signIn("github", { redirectTo: "/" })}
           >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path
