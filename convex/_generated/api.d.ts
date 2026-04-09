466bd8eab4461088009a086b5790ce196ded7169
Tsdebug <taworkspace01@gmail.com>
2026-04-09 14:43:30 +0530

    fixed github button works

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -12,6 +12,7 @@ import type * as auth from "../auth.js";
 import type * as courses_mutations from "../courses/mutations.js";
 import type * as courses_queries from "../courses/queries.js";
 import type * as http from "../http.js";
+import type * as seed from "../seed.js";
 import type * as teachers_queries from "../teachers/queries.js";
 import type * as users from "../users.js";
 import type * as users_mutations from "../users/mutations.js";
@@ -28,6 +29,7 @@ declare const fullApi: ApiFromModules<{
   "courses/mutations": typeof courses_mutations;
   "courses/queries": typeof courses_queries;
   http: typeof http;
+  seed: typeof seed;
   "teachers/queries": typeof teachers_queries;
   users: typeof users;
   "users/mutations": typeof users_mutations;
