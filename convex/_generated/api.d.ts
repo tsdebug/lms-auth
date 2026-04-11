56b3dfded44b5e2a6d1445e927c3f5e6f95b78d9
Tsdebug <taworkspace01@gmail.com>
2026-04-11 14:36:13 +0530

    Added proper theme

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,7 +14,6 @@ import type * as courses_mutations from "../courses/mutations.js";
 import type * as courses_queries from "../courses/queries.js";
 import type * as http from "../http.js";
 import type * as seed from "../seed.js";
-import type * as teachers_queries from "../teachers/queries.js";
 import type * as users from "../users.js";
 import type * as users_mutations from "../users/mutations.js";
 import type * as users_queries from "../users/queries.js";
@@ -32,7 +31,6 @@ declare const fullApi: ApiFromModules<{
   "courses/queries": typeof courses_queries;
   http: typeof http;
   seed: typeof seed;
-  "teachers/queries": typeof teachers_queries;
   users: typeof users;
   "users/mutations": typeof users_mutations;
   "users/queries": typeof users_queries;
