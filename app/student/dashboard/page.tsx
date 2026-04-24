5cffba49cfba16903fd7a4896db942a607eac43b
Tsdebug <taworkspace01@gmail.com>
2026-04-24 20:41:07 +0530

    Made these pages client to solve hydration issues

--- app/student/dashboard/page.tsx
+++ app/student/dashboard/page.tsx
@@ -1,12 +1,12 @@
+"use client"
+
 import * as React from "react"
 import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
-import data from "./data.json"
-import { studentColumns } from "./columns"
+import { StudentCoursesTable } from "./StudentCoursesTable"
 
 export default function StudentDashboardPage() {
   return (
@@ -28,7 +28,7 @@ export default function StudentDashboardPage() {
               <div className="px-4 lg:px-6">
                 <ChartAreaInteractive />
               </div>
-              <DataTable data={data} columns={studentColumns} />
+              <StudentCoursesTable />
             </div>
           </div>
         </div>
