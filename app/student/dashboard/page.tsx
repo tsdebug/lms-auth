5c4cff7041a162cebc05ae2dcc193158fc9ba798
Tsdebug <taworkspace01@gmail.com>
2026-08-29 17:44:29 +0530

    Removed arbitary graph from the teachers' and students' dashboard

--- app/student/dashboard/page.tsx
+++ app/student/dashboard/page.tsx
@@ -23,7 +23,7 @@ export default function StudentDashboardPage() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards role="student" />
               <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+                
               </div>
             </div>
           </div>
--- app/teacher/dashboard/page.tsx
+++ app/teacher/dashboard/page.tsx
@@ -21,7 +21,7 @@ export default function TeacherDashboardPage() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards role="teacher" />
               <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+      
               </div>
             </div>
           </div>
